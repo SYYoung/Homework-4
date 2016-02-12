05485e9f98d3bf0ac3056481b4d75e8d2cf027ac
SYYoung <r38411@yahoo.com>
2016-02-12 23:47:45 +0800

    #3: first version

--- best.R
+++ best.R
@@ -32,3 +32,47 @@ best <- function(state, outcome){
     ##best_list <- tapply(outcome_data[,11], outcome_data$State, min)
 
 }
+
+rankhospital <- function(state, outcome, num="best") {
+  ## Read outcome data
+  outcome_data <- read.csv("outcome-of-care-measures.csv",
+                           colClasses="character")
+  print(unique(outcome_data$State))
+  
+  ## check that state and outcome are valid
+  state_list <- unique(outcome_data$State)
+  outcome_list <- c("heart attack", "heart failure", "pneumonia")
+  match_list <-c("heart attack"="Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack",
+                 "heart failure"="Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure",
+                 "pneumonia"="Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+  
+  if (sum(state_list == state) == 0) {
+    # the state name does not match
+    stop("invalid state")
+  }
+  if (sum(outcome_list == outcome) == 0) {
+    # the outcome name does not match 
+    stop("invalid outcome")
+  }
+  
+  ## based on the outcome, get the list
+  state_hosp_list <- subset(outcome_data, State==state)
+  rank_list <- sort(state_hosp_list[,match_list[outcome]])
+  
+  if (num == "best") {
+    inc_list <- rank_list[1,]
+  }
+  else if (num == "worst"){
+    inc_list <- rank_list[nrow(inc_list),]
+  }
+  else if (num > nrow(inc_list)) 
+    stop("number is too large")
+  else {
+    inc_list <- rank_list[number,]
+  }
+  
+
+  print(inc_list[,"Hospital.Name"])  
+  ## Return hospital name in that state with the given rank
+  ## 30-day death rate
+}
