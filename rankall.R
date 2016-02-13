63c11e025b6c994800aae0e6d683b1bfbc485bea
SY Young <r38411@yahoo.com>
2016-02-13 19:34:55 +0800

    1. clean up the code. 2. list the output as 2 column array

--- rankall.R
+++ rankall.R
@@ -4,15 +4,15 @@ rankStateHospital <- function(outcome_data, state, outcome, num) {
                    "pneumonia"="Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
     ## based on the outcome, get the list
     state_hosp_list <- subset(outcome_data, State==state)
-    print(state_hosp_list[,"Hospital.Name"])
-    print(state_hosp_list[,match_list[outcome]])
+ 
     ## convert the character inputs to numeric
     state_hosp_list[,match_list[outcome]] <- as.numeric(state_hosp_list[,match_list[outcome]])
     
+    ## order the list: first based on the outcome value, then the alphabetical order
+    ## rank_list[1] stores the lowest number of outcome value
     rank_list <- order(state_hosp_list[,match_list[outcome]], 
                        state_hosp_list$Hospital.Name,
                        na.last=NA)
-    print(paste("length of rank_list:",length(rank_list),rank_list))
     num_item <- NROW(rank_list)
     if (num == "best") {
         name_list <- state_hosp_list[rank_list[1],"Hospital.Name"]
@@ -25,7 +25,7 @@ rankStateHospital <- function(outcome_data, state, outcome, num) {
     else {
         name_list <- state_hosp_list[rank_list[num],"Hospital.Name"]
     }    
-    print(paste("class of name_list:",class(name_list), "name_list :",name_list))
+    
     name_list
 }
 
@@ -37,7 +37,6 @@ rankall <- function(outcome, num="best") {
   
   ## check that state and outcome are valid
   state_list <- sort(unique(outcome_data$State))
-  print(state_list)
   outcome_list <- c("heart attack", "heart failure", "pneumonia")
 
   
@@ -47,13 +46,13 @@ rankall <- function(outcome, num="best") {
   }
   
   ## for each state, find the hospital of the given rank
+  hosp_list <- character(length(state_list))
   all_state_list = list(Hospital.Name=character(length(state_list)), State=state_list)
-  print("all_state_list")
-  print(all_state_list)
   for (i in seq_along(state_list)) {
-      all_state_list$Hospital.Name[i] = rankStateHospital(outcome_data,state_list[i],outcome, num)
-      print(paste("i=",i,"all_state_list:",all_state_list$Hospital.Name[i] ))
+      hosp_list[i] = rankStateHospital(outcome_data,state_list[i],outcome, num)
   }
+  
+  all_state_list <- cbind(hosp_list, state_list)
   
   all_state_list
 }
